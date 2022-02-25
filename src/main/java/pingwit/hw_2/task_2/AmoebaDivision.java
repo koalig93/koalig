a3e7235b22811f2ab020e7b5dd0787c9e6f2fad8
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-25 21:45:05 +0100

    Update AmoebaDivision.java

--- src/main/java/pingwit/hw_2/task_2/AmoebaDivision.java
+++ src/main/java/pingwit/hw_2/task_2/AmoebaDivision.java
@@ -3,8 +3,8 @@ package pingwit.hw_2.task_2;
 public class AmoebaDivision {
     public static void main(String[] args) {
         long amoebaNum = 1;
-        for (int i = 3; i <= 24; i += 3) {
-            amoebaNum <<= 1;
+        for (int i = 3; i <= 24; i += 3) { // 3 стоит вынести в переменную
+            amoebaNum <<= 1; // зачем усложняешь? amoebaNum *= 2 проще же читать   
             System.out.println("number of amoebs after " + i + " hours: " + amoebaNum);
         }
     }
