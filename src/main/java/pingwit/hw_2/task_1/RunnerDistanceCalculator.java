6afd9e1d1e7c780782718261d02664d942c8d21f
Alexander Kozlov <alexander.kozlov@duallab.com>
2022-02-25 13:21:24 +0300

    Add calculation by formula using BigDecimal for hw_1/task_1

--- src/main/java/pingwit/hw_2/task_1/RunnerDistanceCalculator.java
+++ src/main/java/pingwit/hw_2/task_1/RunnerDistanceCalculator.java
@@ -1,5 +1,7 @@
 package pingwit.hw_2.task_1;
 
+import java.math.BigDecimal;
+
 public class RunnerDistanceCalculator {
     public static void main(String[] args) {
         int initialDistance = 10;
@@ -7,6 +9,9 @@ public class RunnerDistanceCalculator {
         int dayNumber = 7;
         double totalDistance = calculateTotalDistance(initialDistance, dailyIncPercentage, dayNumber);
         System.out.println("total distance: " + totalDistance + " km");
+
+        totalDistance = calculateTotalDistanceByFormula(initialDistance, dailyIncPercentage, dayNumber);
+        System.out.println("check total distance: " + totalDistance + " km");
     }
 
     private static double calculateTotalDistance(double initialDistance, int dailyIncPercentage, int dayNumber) {
@@ -18,5 +23,12 @@ public class RunnerDistanceCalculator {
             totalDistance += currentDayDistance;
         }
         return totalDistance;
+    }
+
+    // very slow
+    private static double calculateTotalDistanceByFormula(double initialDistance, int dailyIncPercentage, int dayNumber) {
+        double dailyInc = 0.01 * dailyIncPercentage;
+        double pow = BigDecimal.valueOf(1 + dailyInc).pow(dayNumber).doubleValue();
+        return initialDistance * (pow - 1) / dailyInc;
     }
 }
