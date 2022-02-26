9b226caa0bce7270ec1e5fc5f761ec686a88a570
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-26 09:05:19 +0100

    Update DistanceTable.java

--- src/main/java/pingwit/hw_2/task_5/DistanceTable.java
+++ src/main/java/pingwit/hw_2/task_5/DistanceTable.java
@@ -1,9 +1,9 @@
 package pingwit.hw_2.task_5;
 
 public class DistanceTable {
-    private static final double CM_PER_INCH = 2.54;
+    private static final double CM_PER_INCH = 2.54; // отделяй константы абзацем
     public static void main(String[] args) {
-        for (int inchDist = 1; inchDist <= 20; ++inchDist) {
+        for (int inchDist = 1; inchDist <= 20; ++inchDist) { // 20 - магических чисел не должно быть, выноси в переменную
             System.out.println(inchDist + " inch = " + inchDist * CM_PER_INCH + " cm");
         }
     }
