c940c8ea25acd8fb57401cfb48f65267ed27da91
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-26 09:10:12 +0100

    Update OddSum.java

--- src/main/java/pingwit/hw_2/task_7/OddSum.java
+++ src/main/java/pingwit/hw_2/task_7/OddSum.java
@@ -3,10 +3,10 @@ package pingwit.hw_2.task_7;
 public class OddSum {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <= 99; i += 2) {
+        for (int i = 1; i <= 99; i += 2) { // 99 в переменную
             sum += i;
         }
         System.out.println("sum = " + sum);
-        System.out.println("the same as " + 50 * (1 + 99) / 2);
+        System.out.println("the same as " + 50 * (1 + 99) / 2); // 99 стоит тоже вынести в переменную, чтобы привязать проверку к данным
     }
 }
