68c1089c3a1c5b3c0a3719be1a273f211b1c9674
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-26 09:11:50 +0100

    Update OddEvenTotalSum.java

--- src/main/java/pingwit/hw_2/task_8/OddEvenTotalSum.java
+++ src/main/java/pingwit/hw_2/task_8/OddEvenTotalSum.java
@@ -1,9 +1,9 @@
 package pingwit.hw_2.task_8;
 
 public class OddEvenTotalSum {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // старайся отделать абзацами логические блоки, так код проще читать
         int evenSum = 0;
-        int oddSum = 0;
+        int oddSum = 0; // здесь лучше абзац
         for (String arg: args) {
             int number = Integer.parseInt(arg);
             if (number % 2 == 0) {
@@ -11,7 +11,7 @@ public class OddEvenTotalSum {
             } else {
                 oddSum += number;
             }
-        }
+        } // здесь лучше абзац
         System.out.println("sum of evens: " + evenSum);
         System.out.println("sum of odds:  " + oddSum);
         System.out.println("total sum:    " + (evenSum + oddSum));
