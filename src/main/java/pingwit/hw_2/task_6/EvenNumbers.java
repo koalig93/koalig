da66040f1899b6c8c0024d392e75d6a0689634a5
Alexander Fomin <RezviyBelorus@gmail.com>
2022-02-26 09:05:47 +0100

    Update EvenNumbers.java

--- src/main/java/pingwit/hw_2/task_6/EvenNumbers.java
+++ src/main/java/pingwit/hw_2/task_6/EvenNumbers.java
@@ -2,7 +2,7 @@ package pingwit.hw_2.task_6;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-        for (int i = 2; i <= 100; i += 2) {
+        for (int i = 2; i <= 100; i += 2) { // 100 - лучше в переменную
             System.out.println(i);
         }
     }
