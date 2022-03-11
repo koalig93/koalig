5bba1430c8992851894245e5b425ab3aea6bbcda
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-11 16:55:09 +0100

    Update EvenElementsCopy.java

--- src/main/java/pingwit/hw_3/task_6/EvenElementsCopy.java
+++ src/main/java/pingwit/hw_3/task_6/EvenElementsCopy.java
@@ -5,19 +5,19 @@ import pingwit.hw_3.TestSet;
 
 public class EvenElementsCopy {
 
-
+// эта строка лишняя, ее лучше удалить
     public static void main(String[] args) {
         for (int[] row : TestSet.INTEGER_ARRAYS) {
             System.out.println(Arrays.toString(getEvenElements(row)));
         }
     }
 
-    private static int[] getEvenElements(int[] numbers) {
+    private static int[] getEvenElements(int[] numbers) { // задача пока решена неверно
         if (numbers == null) {
             return null;
         }
 
-        int[] result = new int[numbers.length / 2];
+        int[] result = new int[numbers.length / 2]; // а почему ты решил что конечный массив будет х2 короче начального?
 
         int j = 0;
         for (int i = 1; i < numbers.length; i += 2) {
