b9bcec0a5fcf9f8a5856287c62990c684d4d4232
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-11 08:55:10 +0100

    Update ZeroOddElements.java

--- src/main/java/pingwit/hw_3/task_3/ZeroOddElements.java
+++ src/main/java/pingwit/hw_3/task_3/ZeroOddElements.java
@@ -5,7 +5,7 @@ import pingwit.hw_3.TestSet;
 
 public class ZeroOddElements {
     public static void main(String[] args) {
-        for (int[] row : TestSet.INTEGER_ARRAYS) {
+        for (int[] row : TestSet.INTEGER_ARRAYS) { // TestSet.INTEGER_ARRAYS класс с паблик константой - плохое решение, лучше использовать файл или enum
             replaceOddsByZero(row);
             System.out.println(Arrays.toString(row));
         }
@@ -17,7 +17,8 @@ public class ZeroOddElements {
             return;
         }
 
-        // We assume that the first element of array is odd, the second is even and so on
+        // We assume that the first element of array is odd, the second is even and so on - оставлять комментарии в коде плохая примета
+        // odd - нечетный, even - четный
         for (int i = 0; i < numbers.length; i += 2) {
             numbers[i] = 0;
         }
