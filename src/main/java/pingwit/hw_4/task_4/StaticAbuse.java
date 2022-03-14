f792042d8edf721e0b31d15defcc0842428435cc
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-14 21:32:41 +0100

    Update StaticAbuse.java

--- src/main/java/pingwit/hw_4/task_4/StaticAbuse.java
+++ src/main/java/pingwit/hw_4/task_4/StaticAbuse.java
@@ -2,6 +2,7 @@ package pingwit.hw_4.task_4;
 
 import pingwit.hw_4.task_1.Person;
 
+// +
 public class StaticAbuse {
     public static void main(String[] args) {
         Person newBorn = new Person();
