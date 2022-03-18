1b5bb2e9d385274c6ba280982d8e41d9b7fe0e30
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-18 08:12:15 +0100

    Update WarehouseMain.java

--- src/main/java/pingwit/hw_5/task_2/WarehouseMain.java
+++ src/main/java/pingwit/hw_5/task_2/WarehouseMain.java
@@ -8,6 +8,12 @@ import pingwit.hw_5.task_1.Laptop;
 public class WarehouseMain {
 
     public static void main(String[] args) {
+        /*
+        Computer[] myComputers = {
+                new Laptop(UUID.randomUUID().toString(), "MSI"),
+                new Laptop(UUID.randomUUID().toString(), "MacBook")
+                };
+        */
         Computer[] myComputers = {new Laptop(UUID.randomUUID().toString(), "MSI"),
                 new Laptop(UUID.randomUUID().toString(), "MacBook")};
         Warehouse<Computer> warehouse = new Warehouse<>(myComputers);
