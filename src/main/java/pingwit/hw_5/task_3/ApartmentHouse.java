e6de8a9fc773314e9bda19a55b9ae8567b185302
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-18 08:31:17 +0100

    Update ApartmentHouse.java

--- src/main/java/pingwit/hw_5/task_3/ApartmentHouse.java
+++ src/main/java/pingwit/hw_5/task_3/ApartmentHouse.java
@@ -2,7 +2,7 @@ package pingwit.hw_5.task_3;
 
 public class ApartmentHouse extends House {
 
-    private static final int RENT_COEFF = 20;
+    private static final int RENT_COEFF = 20; // для демо варианта ок, но я думаю такой коеффициент на стоит делать одинаковым для всех объектов
 
     public ApartmentHouse(Integer numFloors, Integer numTenants) {
         super(numFloors, numTenants);
