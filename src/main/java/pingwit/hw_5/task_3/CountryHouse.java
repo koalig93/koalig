09d541fc9e901aee1fa20411898bc9fcdc040f06
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-18 08:31:41 +0100

    Update CountryHouse.java

--- src/main/java/pingwit/hw_5/task_3/CountryHouse.java
+++ src/main/java/pingwit/hw_5/task_3/CountryHouse.java
@@ -2,7 +2,7 @@ package pingwit.hw_5.task_3;
 
 public class CountryHouse extends House {
 
-    private static final int RENT_COEFF = 25;
+    private static final int RENT_COEFF = 25; // для демо варианта ок, но я думаю такой коеффициент на стоит делать одинаковым для всех объектов
 
     public CountryHouse(Integer numFloors, Integer numTenants) {
         super(numFloors, numTenants);
