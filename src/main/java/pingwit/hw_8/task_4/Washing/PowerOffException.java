982818fc72bfa8c3435edcc0f43803d8f8bdd0dd
Alexander Fomin <RezviyBelorus@gmail.com>
2022-03-30 08:52:57 +0200

    Update PowerOffException.java

--- src/main/java/pingwit/hw_8/task_4/Washing/PowerOffException.java
+++ src/main/java/pingwit/hw_8/task_4/Washing/PowerOffException.java
@@ -1,4 +1,4 @@
-package pingwit.hw_8.task_4.Washing;
+package pingwit.hw_8.task_4.Washing; // Washing - название пакета с маленькой буквы
 
 public class PowerOffException extends Exception {
     public PowerOffException() {
