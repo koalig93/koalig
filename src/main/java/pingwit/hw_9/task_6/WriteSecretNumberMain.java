6cde38a093f3a4e17c788811d70242c318b4ac83
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-01 08:27:02 +0200

    Update WriteSecretNumberMain.java

--- src/main/java/pingwit/hw_9/task_6/WriteSecretNumberMain.java
+++ src/main/java/pingwit/hw_9/task_6/WriteSecretNumberMain.java
@@ -3,6 +3,7 @@ package pingwit.hw_9.task_6;
 import java.io.FileWriter;
 import java.io.IOException;
 
+// еще можно было запомнить с какого байта началась запись твоей фразы в файл и затем начать читать с этого момента
 public class WriteSecretNumberMain {
     public static void main(String[] args) {
         try (FileWriter fw = new FileWriter(Path.IMAGE.toString(), true)) {
