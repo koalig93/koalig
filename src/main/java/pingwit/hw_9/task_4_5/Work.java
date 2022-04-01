3c7d0125e5ce0773ff059814427313b4ad610a8d
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-01 08:20:42 +0200

    Update Work.java

--- src/main/java/pingwit/hw_9/task_4_5/Work.java
+++ src/main/java/pingwit/hw_9/task_4_5/Work.java
@@ -6,7 +6,7 @@ public class Work implements Serializable {
 
     private static final long serialVersionUID = -3699196701349241697L;
 
-    public String name;
+    public String name; // public -> private Не должно быть у класса public полей
     private Integer experience;
 
     public Work(String name, Integer experience) {
