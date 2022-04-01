3f0067290fe5ee2e0abae9d4928e8c5927302816
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-01 08:24:40 +0200

    Update Path.java

--- src/main/java/pingwit/hw_9/task_6/Path.java
+++ src/main/java/pingwit/hw_9/task_6/Path.java
@@ -1,6 +1,7 @@
 package pingwit.hw_9.task_6;
 
 // внезапно enum в hw_9 :)
+// потренироваться конечно можно, но вообще енам использовать для хранения пути к файлу вряд ли хорошая идея, т.к. он часто может меняться
 public enum Path {
     IMAGE("src/main/java/pingwit/hw_9/task_6/shrek.png");
 
