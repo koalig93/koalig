b659f19c08bd4c567888c9d79e8173dbe0dc679a
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-01 08:23:36 +0200

    Update Path.java

--- src/main/java/pingwit/hw_9/task_6/Path.java
+++ src/main/java/pingwit/hw_9/task_6/Path.java
@@ -1,9 +1,10 @@
 package pingwit.hw_9.task_6;
 
+// внезапно enum в hw_9 :)
 public enum Path {
     IMAGE("src/main/java/pingwit/hw_9/task_6/shrek.png");
 
-    String path;
+    String path; // private String path
 
     Path(String path) {
         this.path = path;
